7266e8ec2e1c52c77fb20a52aab856f815dee88c
parwalrahul <parwalrahul@yahoo.co.in>
2016-09-20 18:53:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,6 +13,16 @@ app.get('/article-one', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
+app.get('/article-two', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
+});
+
+
+app.get('/article-three', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
+});
+
+
 app.get('/first', function (req, res) {
   res.send('You are on the URL for first article');
 });
